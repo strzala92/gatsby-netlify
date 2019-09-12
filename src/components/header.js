a7c980f1019e6971f75df60a5df149609fd709fd
sstrzelecki <s.strzelecki@pap.pl>
2019-09-12 16:07:59 +0200

    Add header and  navigation

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,21 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import { Container, Row, Col} from 'reactstrap'
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
+  <header className="masthead">
+    <div className="overlay"></div>
+    <Container>
+      <Row>
+        <Col lg={8} md={10} className="mx-auto">
+          <div className="site-heading">
+            <h1>Clean Blog</h1>
+            <span className="subheading">{ siteTitle }</span>
+          </div>
+          </Col>
+      </Row>
+     </Container>
   </header>
 )
 
