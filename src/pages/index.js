cc1a7e88bfcbea644187ddf84b7e04713739d362
sstrzelecki <s.strzelecki@pap.pl>
2019-09-12 14:29:33 +0200

    Add Sass

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,8 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css"
+import "../index.scss"
 
 const IndexPage = () => (
   <Layout>
